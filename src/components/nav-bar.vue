9d759e77e582918ede567b3ecc975bef58330c06
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2023-07-29 22:17:52 +0000

    update navbar

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-full flex flex-col lg:flex-row justify-between p-8 drop-shadow-lg bg-[#f5e5d1]">
+    <div class="w-full flex flex-col lg:flex-row justify-evenly p-8 drop-shadow-lg bg-[#f5e5d1]">
       <div class="font-bold text-2xl items-center">
         <router-link to="/">Slite</router-link>
       </div>
       <div class="flex flex-col  lg:flex-row  items-center lg:ml-8">
-        <div class="flex font-medium py-2 ">
+        <div class="flex lg:flex-row flex-col font-medium py-2 ">
           <router-link class="my-4 lg:my-0 lg:mx-4" to="/">Product</router-link>
           <router-link class="my-4 lg:my-0 lg:mx-4" to="/">Template</router-link>
           <router-link class="my-4 lg:my-0 lg:mx-4" to="/">Pricing</router-link>
@@ -12,7 +12,7 @@
           <router-link class="my-4 lg:my-0 lg:mx-4" to="/">Blog</router-link>
           <router-link class="my-4 lg:my-0 lg:mx-4" to="/">Sign in</router-link>
         </div>
-        <div class="flex mt-2 lg:mt-0">
+        <div class="flex lg:flex-row flex-col  mt-2">
           <router-link>
             <buttonBlue text="Request a demo" />
           </router-link>
